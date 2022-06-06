474cb03859a0210984220e747154e72b2800a2c9
jobdk <johnybudnik+git@gmail.com>
2022-06-06 14:32:21 +0200

    added post mapping

--- src/main/java/com/forschungsprojekt/persister/controller/PersistenceController.java
+++ src/main/java/com/forschungsprojekt/persister/controller/PersistenceController.java
@@ -1,7 +1,9 @@
 package com.forschungsprojekt.persister.controller;
 
 import com.forschungsprojekt.persister.data.ImageInformation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
