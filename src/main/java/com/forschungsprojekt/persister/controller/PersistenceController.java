ba7066480c7eb2ea82e3535a75ae2dfebebf5d1f
LucaWeinmann <luca.we98@gmx.de>
2022-06-15 14:51:37 +0200

    Implemented Docker File Management

--- src/main/java/com/forschungsprojekt/persister/controller/PersistenceController.java
+++ src/main/java/com/forschungsprojekt/persister/controller/PersistenceController.java
@@ -3,6 +3,7 @@ package com.forschungsprojekt.persister.controller;
 import com.forschungsprojekt.persister.data.ImageInformation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
